fe437445496eb9179038865a24c493e8c2ad605a
bdenckla <ben_denckla@yahoo.com>
2011-11-23 12:35:43 -0800

    Update Ubuntu-setup.sh

--- Ubuntu-setup.sh
+++ Ubuntu-setup.sh
@@ -4,6 +4,14 @@ sudo apt-get -y install \
     php-elisp \
     #
 
+# Download id_rsa
+
+[ -d ~/.ssh ] || \
+   ( mkdir ~/.ssh && chmod go-rwx ~/.ssh )
+
+[ -f ~/.ssh/id_rsa ] || \
+    ( mv ~/Downloads/id_rsa ~/.ssh && chmod go-rwx ~/.ssh/id_rsa )
+
 cd ~ && git clone git@github.com:bdenckla/github-misc.git
 
 for f in gitconfig emacs; do
