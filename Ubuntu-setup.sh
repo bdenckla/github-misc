0833ddb5b2e56afc8ae6d977183b5793544fb3c5
Ben Denckla <bdenckla@alum.mit.edu>
2011-11-23 14:51:56 -0800

    more Ubuntu-setup.sh work

--- Ubuntu-setup.sh
+++ Ubuntu-setup.sh
@@ -1,5 +1,3 @@
-set -e
-
 # This script assumes that the id_rsa file has already been downloaded
 # to ~/Downloads.
 
@@ -7,29 +5,45 @@ sudo apt-get -y install \
     git \
     emacs \
     php-elisp \
-    #
+    git-el \
+    || \
+    exit
 
 [ -d ~/.ssh ] || \
-   ( mkdir ~/.ssh && chmod go-rwx ~/.ssh )
+   ( mkdir ~/.ssh && chmod go-rwx ~/.ssh ) \
+    || \
+    exit
 
 [ -f ~/.ssh/id_rsa ] || \
-    ( mv ~/Downloads/id_rsa ~/.ssh && chmod go-rwx ~/.ssh/id_rsa )
+    ( mv ~/Downloads/id_rsa ~/.ssh && chmod go-rwx ~/.ssh/id_rsa ) \
+    || \
+    exit
 
 [ -d ~/github-misc ] || \
-    git clone git@github.com:bdenckla/github-misc.git ~/github-misc
+    git clone git@github.com:bdenckla/github-misc.git ~/github-misc \
+    || \
+    exit
 
 for f in gitconfig emacs; do
-    ln -s ~/github-misc/dot-$f ~/.$f;
+    [ -h ~/.$f ] \
+    || \
+    ln -s ~/github-misc/dot-$f ~/.$f \
+    || \
+    exit
 done
 
 which \
     git \
     emacs \
-    #
+    || \
+    exit
 
 ls -l \
     ~/.ssh \
     ~/github-misc \
     ~/.emacs \
     ~/.gitconfig \
-    #
+    || \
+    exit
+
+echo $0 ending with success.
