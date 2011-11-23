9a3478df76a17544299d81fc078b244a20ef6721
Ben Denckla <bdenckla@alum.mit.edu>
2011-11-23 10:32:32 -0800

    add git clone of github-misc and linking of dot files

--- Ubuntu-setup.sh
+++ Ubuntu-setup.sh
@@ -1,4 +1,11 @@
 sudo apt-get -y install \
+    git \
     emacs \
     php-elisp \
     #
+
+cd ~ && git clone git@github.com:bdenckla/github-misc.git
+
+for f in gitconfig emacs; do
+    ln -s ~/github-misc/dot-$f ~/.$f;
+done
