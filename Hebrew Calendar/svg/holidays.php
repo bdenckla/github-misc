02768bf3230cfa57930ba0a893d384bca5792b6d
Ben Denckla <bdenckla@alum.mit.edu>
2013-09-26 17:04:36 -0700

    make Species class; show year length

--- Hebrew Calendar/svg/holidays.php
+++ Hebrew Calendar/svg/holidays.php
@@ -1,16 +1,6 @@
 #!/usr/bin/php -q
 <?php
 
-function pa() // Partially Apply
-{
-  $origArgs = func_get_args();
-  return function() use ( $origArgs )
-    {
-      $allArgs = array_merge( $origArgs, func_get_args() );
-      return call_user_func_array( 'call_user_func', $allArgs );
-    };
-}
-
 function holidays()
 {
   // month, day, name
@@ -64,6 +54,29 @@ function mn_max() { return 12; }
 
 function mn_start() { return mn_ad(); }
 
+function year_species()
+{
+  return array
+    (
+     new Species( -1, 0 ),
+     new Species(  0, 0 ),
+     new Species(  1, 0 ),
+     new Species( -1, 1 ),
+     new Species(  0, 1 ),
+     new Species(  1, 1 ),
+     );
+}
+
+function pa() // Partially Apply
+{
+  $origArgs = func_get_args();
+  return function() use ( $origArgs )
+    {
+      $allArgs = array_merge( $origArgs, func_get_args() );
+      return call_user_func_array( 'call_user_func', $allArgs );
+    };
+}
+
 function month_name( $month_number )
 {
   $a = month_name_array();
@@ -71,35 +84,15 @@ function month_name( $month_number )
   return $a[ $month_number ];
 }
 
-/* day_adj: -1, 0, or 1 (short Kislev, normal, long Cheshvan)
- * month_adj: 0 or 1 (non-leap or leap)
- *
- * I.e. the 6 possible year lengths have the following correspondences
- * to day_adj/month_adj pairs:
- *
- * 353, 354, 355 correspond to (-1,0), (0,0), (1,0)
- * 383, 384, 385 correspond to (-1,1), (0,1), (1,1)
-*/
-
-function days_per_month( $month_number, array $species )
+function days_per_month( $month_number, Species $species )
 {
-  list( $day_adj, $month_adj ) = $species;
+  list( $day_adj, $month_adj ) = array( $species->day_adj, $species->month_adj );
 
   $e = NULL;
 
   if ( ! array_key_exists( $month_number, month_name_array() ) )
     {
       $e = array( 'unexpected month number', $month_number );
-    }
-
-  if ( ! in_array( $day_adj, array( -1, 0, 1 ) ) )
-    {
-      $e = array( 'unexpected day_adj', $day_adj );
-    }
-
-  if ( ! in_array( $month_adj, array( 0, 1 ) ) )
-    {
-      $e = array( 'unexpected month_adj', $month_adj );
     }
 
   if ( $e )
@@ -125,7 +118,7 @@ function days_per_month( $month_number, array $species )
   return 29 + $month_number % 2;
 }
 
-function accumulate_days( $species, $acc, $month_number )
+function accumulate_days( Species $species, $acc, $month_number )
 {
   return $acc + days_per_month( $month_number, $species );
 }
@@ -137,7 +130,7 @@ function previous_month( $month_number )
     : $month_number - 1;
 }
 
-function day_of_year_of_month( $species, $month_number )
+function day_of_year_of_month( Species $species, $month_number )
 {
   if ( $month_number == mn_start() )
     {
@@ -162,47 +155,81 @@ function day_of_year_of_month( $species, $month_number )
   return array_reduce( $mns, $pa, 0 );
 }
 
-function day_of_year_of_holiday( $species, array $holiday )
+function days_per_year( Species $species )
+{
+  $mns = range( mn_min(), mn_max() );
+
+  $pa = pa( 'accumulate_days', $species );
+
+  return array_reduce( $mns, $pa, 0 );
+}
+
+function day_of_year_of_holiday( Species $species, array $holiday )
 {
   list ( $month, $day_of_month, $name ) = $holiday;
 
   $doyom = day_of_year_of_month( $species, $month );
 
-  $doy = $doyom + $day_of_month;
-
-  return $doy;
+  return $doyom + $day_of_month;
 }
 
-function day_of_year_of_holidays( $species )
+function day_of_year_of_holidays( Species $species )
 {
   $pa = pa( 'day_of_year_of_holiday', $species );
 
-  $a = array_map( $pa, holidays() );
-
-  return $a;
+  return array_map( $pa, holidays() );
 }
 
-function doyoh_sort( $doyoh1, $doyoh2 )
+function holidays_and_length( Species $species )
 {
-  list( $holiday1, $doy1 ) = $doyoh1;
-  list( $holiday2, $doy2 ) = $doyoh2;
+  return array
+    (
+     'length' => days_per_year( $species ),
+     'holidays' => day_of_year_of_holidays( $species ),
+     );
+}
 
-  return $doy1 > $doy2;
+/* day_adj: -1, 0, or 1 (short Kislev, normal, long Cheshvan)
+ * month_adj: 0 or 1 (non-leap or leap)
+ *
+ * I.e. the 6 possible year lengths have the following correspondences
+ * to day_adj/month_adj pairs:
+ *
+ * 353, 354, 355 correspond to (-1,0), (0,0), (1,0)
+ * 383, 384, 385 correspond to (-1,1), (0,1), (1,1)
+*/
+
+class Species
+{
+  function __construct( $day_adj, $month_adj )
+  {
+    $e = NULL;
+
+    if ( ! in_array( $day_adj, array( -1, 0, 1 ) ) )
+      {
+        $e = array( 'unexpected day_adj', $day_adj );
+      }
+
+    if ( ! in_array( $month_adj, array( 0, 1 ) ) )
+      {
+        $e = array( 'unexpected month_adj', $month_adj );
+      }
+
+    if ( $e )
+      {
+        throw new ErrorException( var_export( $e, 1 ) );
+      }
+
+    $this->day_adj = $day_adj;
+    $this->month_adj = $month_adj;
+  }
+  public $day_adj;
+  public $month_adj;
 }
 
 function main()
 {
-  $species = array
-    (
-     array( -1, 0 ),
-     array(  0, 0 ),
-     array(  1, 0 ),
-     array( -1, 1 ),
-     array(  0, 1 ),
-     array(  1, 1 ),
-     );
-
-  return array_map( 'day_of_year_of_holidays', $species );
+  return array_map( 'holidays_and_length', year_species() );
 }
 
 var_export( main() );
