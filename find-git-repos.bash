f26639dfa3d431235324806fac5ae0c89e7e90c6
Ben Denckla <bdenckla@alum.mit.edu>
2016-03-31 06:14:41 -0700

    add example use of find git repos

--- find-git-repos.bash
+++ find-git-repos.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Example use:
+#
+#    find $(github-misc/find-git-repos.bash) -name "*.php"
+#
+#    grep -r intersperse $(github-misc/find-git-repos.bash) --include "*.php"
+
 for f in $(find . -maxdepth 2 -name ".gitignore");
 do
     echo $(dirname $f)
